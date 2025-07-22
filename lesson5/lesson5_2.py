5bd04782e76b1e367ca34354c695852250c24899
eileenwang77 <wangew64@gmail.com>
2025-07-22 10:55:45 +0800

    l5_2_c2

--- lesson5/lesson5_2.py
+++ lesson5/lesson5_2.py
@@ -13,6 +13,13 @@ async def main():
         print("n"*40)
         print(result.markdown.raw_markdown[:200])
 
-asyncio.run(main())
+if __name__ == "__main__":
+    #使用 asyncio.run() 來執行 main 函數
+    #這樣可以確保事件循環正確運行
+    #如果在 Jupyter Notebook 中，可能需要使用 nest_asyncio.apply() 來解決事件循環問題
+    # import nest_asyncio
+    # nest_asyncio.apply()  # 解決 asyncio 的事件循環問題
+    
+    asyncio.run(main())
 
 # await main()
