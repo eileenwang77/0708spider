77d15606911e3b78880dd81984f8d09552fed5c3
eileenwang77 <wangew64@gmail.com>
2025-07-22 09:37:30 +0800

    l5_1_n

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -1,9 +1,12 @@
 from playwright.async_api import async_playwright
 import asyncio
+# import nest_asyncio
+
+# nest_asyncio.apply()  # 解決 asyncio 的事件循環問題
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False,slow_mo=5000)
+        browser = await p.chromium.launch(headless=False,slow_mo=1000)
         page = await browser.new_page()
         await page.goto('https://example.com')
         await page.wait_for_selector('p') #等待元素載入
