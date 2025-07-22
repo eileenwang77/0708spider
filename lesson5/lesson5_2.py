8ac516b5f66b9a19a52fa6c7e862fc11ffdfb5e5
eileenwang77 <wangew64@gmail.com>
2025-07-22 10:50:29 +0800

    l5_2_c1

--- lesson5/lesson5_2.py
+++ lesson5/lesson5_2.py
@@ -6,9 +6,12 @@ async def main():
     async with AsyncWebCrawler() as crawler:
         #Run the crawler on a URL
         result = await crawler.arun(url='https://crawl4ai.com')
-
+        print(type(result))
+        print("=")
         #列印取出的結果
         print(result.markdown)
+        print("n"*40)
+        print(result.markdown.raw_markdown[:200])
 
 asyncio.run(main())
 
